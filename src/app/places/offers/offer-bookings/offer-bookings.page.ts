593b1ce8ca2364d1874aa84418608fd473836c94
shrikant patki <shripatki9@gmail.com>
2020-05-08 01:01:48 +0530

    [SP] added offer page

--- src/app/places/offers/offer-bookings/offer-bookings.page.ts
+++ src/app/places/offers/offer-bookings/offer-bookings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-offer-bookings',
@@ -6,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./offer-bookings.page.scss'],
 })
 export class OfferBookingsPage implements OnInit {
-
-  constructor() { }
+  constructor(private router:Router,
+    private navController:NavController) { }
 
   ngOnInit() {
   }
 
+  onAvailOffer(){
+    //this.router.navigateByUrl('/places/tabs/discover');
+   this.navController.navigateBack('/places/tabs/offers');
+  }
 }
