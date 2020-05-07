7005f9e294f3375d962d1e0c023126ae840d12fd
shrikant patki <shripatki9@gmail.com>
2020-05-07 22:44:08 +0530

    [SP] Created project structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,21 @@ const routes: Routes = [
  
   {
     path: '',
-    redirectTo: 'receipe',
+    redirectTo: 'places',
     pathMatch: 'full'
   },
 
   {
     path: 'auth',
     loadChildren: () => import('./auth/auth.module').then( m => m.AuthPageModule)
+  },
+  {
+    path: 'places',
+    loadChildren: () => import('./places/places.module').then( m => m.PlacesPageModule)
+  },
+  {
+    path: 'bookings',
+    loadChildren: () => import('./bookings/bookings.module').then( m => m.BookingsPageModule)
   },
 ];
 
---
+++ src/app/places/offers/offers-routing.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { OffersPage } from './offers.page';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: OffersPage
+  },
+  {
+    path: 'new-offers',
+    loadChildren: () => import('./new-offers/new-offers.module').then( m => m.NewOffersPageModule)
+  },
+  {
+    path: 'edit-offer',
+    loadChildren: () => import('./edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
+  },
+  {
+    path: 'offer-bookings',
+    loadChildren: () => import('./offer-bookings/offer-bookings.module').then( m => m.OfferBookingsPageModule)
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class OffersPageRoutingModule {}
