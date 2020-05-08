26d6db524ee83bde5669e4edf30e7c020dd3340f
shrikant patki <shripatki9@gmail.com>
2020-05-08 23:38:30 +0530

    [SP] Removed second way of adding the sidebar

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
-import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-discover',
@@ -11,13 +10,11 @@ import { MenuController } from '@ionic/angular';
 export class DiscoverPage implements OnInit {
   loadedPlaces:Place[];
   constructor(private placesService:PlacesService,
-    private menuController:MenuController) { }
+    ) { }
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.getplaces();
   }
 
-  onMenuClick(){
-    this.menuController.toggle()
-  }
+  
 }
