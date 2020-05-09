927b12bb6dff7489fff9cfdbf5dd3e9e7f806283
shrikant patki <shripatki9@gmail.com>
2020-05-10 00:05:59 +0530

    [SP] Sliding option added

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Place } from '../../place.model';
 import { ActivatedRoute } from '@angular/router';
 import { PlacesService } from '../../places.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-edit-offer',
@@ -11,10 +12,15 @@ import { PlacesService } from '../../places.service';
 export class EditOfferPage implements OnInit {
   place:Place;
   constructor(private router:ActivatedRoute,
-    private placeService:PlacesService) { }
+    private placeService:PlacesService,
+    private navController:NavController) { }
 
     ngOnInit() {
       this.router.paramMap.subscribe(paramMap=>{
+        if(!paramMap.get("placeId")){
+          this.navController.navigateBack('/places/tabs/offers')
+          return 
+        }
         this.place = this.placeService.getplace(paramMap.get("placeId"))   
       })
     }
--- src/app/places/offers/offers.page.ts
+++ src/app/places/offers/offers.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Place } from '../place.model';
 import { PlacesService } from '../places.service';
+import { NavController, IonItemSliding } from '@ionic/angular';
 
 @Component({
   selector: 'app-offers',
@@ -9,10 +10,18 @@ import { PlacesService } from '../places.service';
 })
 export class OffersPage implements OnInit {
   offers:Place[];
-  constructor(private placesService:PlacesService) { }
+  constructor(
+    private placesService:PlacesService,
+    private navController:NavController
+    ) { }
 
   ngOnInit() {
     this.offers = this.placesService.getplaces();
+  }
+
+  onEdit(placeId:string,slidingItem:IonItemSliding){
+    slidingItem.close();
+    this.navController.navigateForward(['/places/tabs/offers/edit/',placeId])
   }
 
 }
