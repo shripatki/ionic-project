436e80f90c3d8445f6cf5cc36c155fca63c2f3d1
shrikant patki <shripatki9@gmail.com>
2020-05-10 01:26:21 +0530

    [SP] Added segment to the app

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SegmentChangeEventDetail} from '@ionic/core'
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'app-discover',
@@ -18,5 +20,8 @@ export class DiscoverPage implements OnInit {
     this.listOfloadedPlaces = this.loadedPlaces.slice(1);
   }
 
+  onFilterUpdate(event:CustomEvent<SegmentChangeEventDetail>){
+    console.log(event.detail);
+  }
   
 }
