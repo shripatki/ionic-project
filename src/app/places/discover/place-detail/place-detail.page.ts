fee5f1ecc82d867279e9b1bfe099bf1f529f9bfb
shrikant patki <shripatki9@gmail.com>
2020-05-10 01:56:30 +0530

    [SP] Added action sheet controller

--- src/app/places/discover/place-detail/place-detail.page.ts
+++ src/app/places/discover/place-detail/place-detail.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { NavController, ModalController } from '@ionic/angular';
+import { NavController, ModalController, ActionSheetController } from '@ionic/angular';
 import { PlacesService } from '../../places.service';
 import { Place } from '../../place.model';
 import { CreateBookingComponent } from '../../../bookings/create-booking/create-booking.component';
@@ -15,7 +15,8 @@ export class PlaceDetailPage implements OnInit {
   constructor(private router:ActivatedRoute,
     private navController:NavController,
     private placeService:PlacesService,
-    private modalController:ModalController) { }
+    private modalController:ModalController,
+    private actionSheetController:ActionSheetController) { }
 
     ngOnInit() {
       this.router.paramMap.subscribe(paramMap=>{
@@ -29,17 +30,46 @@ export class PlaceDetailPage implements OnInit {
   onBookPlace(){
     //this.router.navigateByUrl('/places/tabs/discover');
   // this.navController.navigateBack('/places/tabs/discover');
-  this.modalController.create({
-    component: CreateBookingComponent,
-    componentProps: {
-      selectedPlace: this.place
-    }
-  }).then(modalEl => {
-    modalEl.present();
-    return modalEl.onDidDismiss();
-  }).then(resultData =>{
-    console.log(resultData);
+  this.actionSheetController.create({
+    header:'Choose a Action',
+    buttons:[
+      {
+        text:'Select Date',
+        handler:()=>{
+          this.openBookingModal('select')
+        }
+
+      },
+      {
+        text:'Random Date',
+        handler:()=>{
+          this.openBookingModal('random')
+        }
+      },
+      {
+        text:'Cancel',
+        role:'cancel'
+      }
+    ]
+  }).then(actionSheetEl=>{
+    actionSheetEl.present();
   })
+ 
+  }
+
+  openBookingModal(mode: 'select'|'random'){
+    console.log(mode);
+    this.modalController.create({
+      component: CreateBookingComponent,
+      componentProps: {
+        selectedPlace: this.place
+      }
+    }).then(modalEl => {
+      modalEl.present();
+      return modalEl.onDidDismiss();
+    }).then(resultData =>{
+      console.log(resultData);
+    })
   }
 
 }
