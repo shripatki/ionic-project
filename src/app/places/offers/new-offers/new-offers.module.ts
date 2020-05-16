d56077c103af52c5773b8be0a8dd9940db6f0ad7
shrikant patki <shripatki9@gmail.com>
2020-05-17 02:32:50 +0530

    [SP] Added ne woffer page with validation

--- src/app/places/offers/new-offers/new-offers.module.ts
+++ src/app/places/offers/new-offers/new-offers.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
@@ -12,6 +12,7 @@ import { NewOffersPage } from './new-offers.page';
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     NewOffersPageRoutingModule
   ],
