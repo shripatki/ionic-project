fb3220061b1c4ca5e73e57c15be2540f51e9ee05
shrikant patki <shripatki9@gmail.com>
2020-05-17 02:46:25 +0530

    [SP] Added edit form module with validation

--- src/app/places/offers/edit-offer/edit-offer.page.ts
+++ src/app/places/offers/edit-offer/edit-offer.page.ts
@@ -3,6 +3,7 @@ import { Place } from '../../place.model';
 import { ActivatedRoute } from '@angular/router';
 import { PlacesService } from '../../places.service';
 import { NavController } from '@ionic/angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-offer',
@@ -11,6 +12,7 @@ import { NavController } from '@ionic/angular';
 })
 export class EditOfferPage implements OnInit {
   place:Place;
+  form:FormGroup;
   constructor(private router:ActivatedRoute,
     private placeService:PlacesService,
     private navController:NavController) { }
@@ -21,8 +23,24 @@ export class EditOfferPage implements OnInit {
           this.navController.navigateBack('/places/tabs/offers')
           return 
         }
-        this.place = this.placeService.getplace(paramMap.get("placeId"))   
+        this.place = this.placeService.getplace(paramMap.get("placeId"))
+        this.form = new FormGroup({
+          title:new FormControl(this.place.title,{
+            updateOn:'blur',
+            validators:[Validators.required]
+          }
+        ),
+        description:new FormControl( this.place.description,{
+            updateOn:'blur',
+            validators:[Validators.required, Validators.maxLength(180)]
+          }
+        )
+        })
       })
+    }
+
+    onEditOffer(){
+      
     }
 
 }
