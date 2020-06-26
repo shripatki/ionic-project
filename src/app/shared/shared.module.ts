876f3c678668793031c318b6049761f3d75f62ca
shripatki9 <shripatki9@gmail.com>
2020-06-27 00:47:44 +0530

    [SP] Added image picker

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from "@angular/common";
 import { LocationPickerComponent } from "./pickers/location-picker/location-picker.component";
 import { MapModalComponent } from "./pickers/map-modal/map-modal.component";
 import { IonicModule } from '@ionic/angular';
+import { ImagePickerComponent } from './pickers/image-picker/image-picker.component';
 
 @NgModule({
-  declarations: [LocationPickerComponent, MapModalComponent],
+  declarations: [LocationPickerComponent, MapModalComponent,ImagePickerComponent],
   imports: [CommonModule, IonicModule],
   exports: [LocationPickerComponent, MapModalComponent],
   entryComponents: [MapModalComponent],
